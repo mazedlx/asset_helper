14c780148fdfca5773aeffbedbb2ed892fdb308b
Christian Leo <mazedlx@gmail.com>
2016-01-28 07:25:23 +0100

    Tests

---
+++ tests/tests.php
@@ -0,0 +1,11 @@
+<?php
+class StackTest extends PHPUnit_Framework_TestCase
+{
+    public function testCreation()
+    {
+        $assetHelper = new AssetHelper\AssetHelper;
+
+        $this->assertContains('test.css', $assetHelper->link_css('test.css'));
+        $this->assertContains('test.js', $assetHelper->link_js('test.js'));
+    }
+}
